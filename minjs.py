e3ce722799849176c12b30b5cf57c95888645cb5
qleelulu <qleelulu@e7e366f2-3051-11df-a3dc-1b164710d598>
2011-03-09 05:04:00 +0000

    多语言

--- minjs.py
+++ minjs.py
@@ -20,5 +20,21 @@ for name in os.listdir('scripts'):
         min = os.path.getsize(source)
         min_size += min
         print '%s %s => %s, %s small' % (source, old, min, old - min)
+
+for dir in os.walk('_locales'):
+    if not dir[2]:
+        continue
+    for name in dir[2]:
+        if name.endswith('.json'):
+            source = dir[0] + '/' + name
+            old = os.path.getsize(source)
+            old_size += old
+            to = source + '.min'
+            os.system('jsmin < %s > %s' % (source, to))
+            os.remove(source)
+            os.rename(to, source)
+            min = os.path.getsize(source)
+            min_size += min
+            print '%s %s => %s, %s small' % (source, old, min, old - min)
         
 print '\r\nChange size %s to %s, %s small' % (old_size, min_size, (old_size - min_size))
