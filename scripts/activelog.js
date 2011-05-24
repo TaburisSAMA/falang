5e27edd78f77e7c43fa7045a4923b94e516e95b5
fengmk2 <fengmk2@e7e366f2-3051-11df-a3dc-1b164710d598>
2011-05-24 18:17:45 +0000

    增加版本号

--- scripts/activelog.js
+++ scripts/activelog.js
@@ -1,16 +1,18 @@
 // 活动记录
 (function() {
 	var ActiveLog = {
-		api: 'http://s8.hk:4444/log',
+		api: 'http://api.yongwo.de:9999/log',
 		app: 'fawave',
 		// chrome.extension.getURL('oauth_cb.html')
 		// "chrome-extension://kdkhkblabbkmgfjfpbijlbijdemdodol/"
 		uid: /\/\/(\w+)\//i.exec(chrome.extension.getURL(''))[1],
+		version: chrome.app.getDetails().version,
 		log: function(active, params, cb) {
 			params = params || {};
-			params.active = active;
+			params.at = active;
 			params.app = this.app;
 			params.uid = this.uid;
+			params.v = this.version;
 			$.get(this.api, params, cb || function() {});
 		}
 	};
