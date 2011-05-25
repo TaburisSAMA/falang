bdd4191e2aa677db8a4e87e818cdc900e584dc3e
fengmk2 <fengmk2@e7e366f2-3051-11df-a3dc-1b164710d598>
2011-05-25 09:53:02 +0000

    尝试获取chrome版本号

--- scripts/activelog.js
+++ scripts/activelog.js
@@ -1,12 +1,15 @@
 // 活动记录
 (function() {
+	if(window.ActiveLog) {
+		return;
+	}
 	var ActiveLog = {
 		api: 'http://api.yongwo.de:9999/log',
 		app: 'fawave',
 		// chrome.extension.getURL('oauth_cb.html')
 		// "chrome-extension://kdkhkblabbkmgfjfpbijlbijdemdodol/"
 		uid: /\/\/(\w+)\//i.exec(chrome.extension.getURL(''))[1],
-		version: chrome.app.getDetails().version,
+		version: 'null',
 		log: function(active, params, cb) {
 			params = params || {};
 			params.at = active;
@@ -24,5 +27,12 @@
 			});
 		}
 	};
+	try {
+		ActiveLog.version = chrome.app.getDetails().version;
+	} catch(e) {
+		$.get(chrome.extension.getURL('manifest.json'), function(info){
+			ActiveLog.version = info.version;
+		}, json);
+	}
 	window.ActiveLog = ActiveLog;
 })();
